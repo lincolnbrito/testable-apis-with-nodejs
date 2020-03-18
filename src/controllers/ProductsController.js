30aa2426d6d9338489174d619300e1bdd1c5a527
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-18 00:35:00 -0300

    test controller

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,5 +1,11 @@
 class ProductsController {
-
+    get(req, res) {
+        return res.send([{
+            name: 'Default product',
+            description: 'product description',
+            price: 100
+        }])
+    }
 }
 
 export default ProductsController;
--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,9 +1,25 @@
 import ProductsController from '../../../src/controllers/ProductsController';
+import sinon from 'sinon';
 
 describe('Controller: Products', () => {
+    const defaultProduct = [{
+        name: 'Default product',
+        description: 'product description',
+        price: 100
+    }];
+
     describe('get() products', () => {
         it('should return a list of products', () => {
-            const producstController = new ProductsController();
+            const request = {};
+            const response = {
+                send: sinon.spy()
+            };
+
+            const productsController = new ProductsController();
+            productsController.get(request, response);
+
+            expect(response.send.called).to.be.true;
+            expect(response.send.calledWith(defaultProduct)).to.be.true;
         });
     });
 });
